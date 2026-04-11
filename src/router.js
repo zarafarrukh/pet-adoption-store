fbbf0c0128e7507d04d76f11cc2da72e48226dc9
Saeem Patel <email@example.com>
2026-04-11 14:16:07 -0400

    Wire up quiz route

--- src/router.js
+++ src/router.js
@@ -2,15 +2,15 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import Homepage from './views/Homepage.vue'
 import BrowsePets from './views/BrowsePets.vue'
 import MerchStore from './views/MerchStore.vue'
-import AdoptionForm from './views/AdoptionForm.vue';
+import AdoptionForm from './views/AdoptionForm.vue'
+import PetQuiz from './views/PetQuiz.vue'
 
 const routes = [
   { path: '/', component: Homepage },
   { path: '/browse', component: BrowsePets },
   { path: '/merch', component: MerchStore },
   { path: '/adopt', component: AdoptionForm },
-  // { path: '/quiz', component: PetQuiz },
-  // { path: '/home', component: Homepage }, 
+  { path: '/quiz', component: PetQuiz },
 ]
 
 export default createRouter({
